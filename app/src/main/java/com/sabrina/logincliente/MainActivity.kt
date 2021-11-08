a37651fbf96eb00a869eaf57826a899be0044ed3
Sabrina <sabrinaarrebillaga@gmail.com>
2021-11-08 15:31:28 -0300

    firebase conect

--- app/src/main/java/com/sabrina/logincliente/MainActivity.kt
+++ app/src/main/java/com/sabrina/logincliente/MainActivity.kt
@@ -2,6 +2,8 @@ package com.sabrina.logincliente
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
